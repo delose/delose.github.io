10d3ddf93352679c35b6cf40970130f8d0038cb7
delose <delossantos.eugene@gmail.com>
2026-01-03 09:10:56 +0800

    change consts

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'Software Engineer Site';
+export const SITE_DESCRIPTION = 'EUGENE SERGIO CASTRO DE LOS SANTOS';
